f5a3a59a534fce8624110a5992acce465d8aef11
entropy-sumit <sumitr600@gmail.com>
2022-02-11 18:21:18 +0530

    [add] sumit added address book with linq uc3 program

--- ADDRESS_BOOK_LINQ/AddressBookSystem.cs
+++ ADDRESS_BOOK_LINQ/AddressBookSystem.cs
@@ -21,6 +21,29 @@ namespace ADDRESS_BOOK_LINQ
             table.Columns.Add("Zip", typeof(string));
             table.Columns.Add("PhoneNumber", typeof(string));
             table.Columns.Add("Email", typeof(string));
+
+            table.Rows.Add("sumit", "rawat", "surajkund", "gorakhpur", "uttar pradesh", "273015", "91411919", "sumitraw@gmail.com");
+            table.Rows.Add("sweta", "singh", "shahi colony", "Patana", "bihar", "44121", "9333934858", "sweta@gmail.com");
+            table.Rows.Add("Vinay", "kumar", "naini", "allahabad", "uttar pradesh", "672206", "9894561230", "vinay@gmail.com.a");
+
+
+        }
+        public void GetAllContacts()
+        {
+            Console.WriteLine("\n\n");
+            foreach (DataRow dr in table.AsEnumerable())
+            {
+                Console.WriteLine("\n\n");
+                Console.WriteLine("FirstName:- " + dr.Field<string>("firstName"));
+                Console.WriteLine("LastName:- " + dr.Field<string>("lastName"));
+                Console.WriteLine("Address:- " + dr.Field<string>("address"));
+                Console.WriteLine("City:- " + dr.Field<string>("city"));
+                Console.WriteLine("State:- " + dr.Field<string>("state"));
+                Console.WriteLine("Zip:- " + dr.Field<string>("zip"));
+                Console.WriteLine("PhoneNumber:- " + dr.Field<string>("phoneNumber"));
+                Console.WriteLine("Email:- " + dr.Field<string>("eMail"));
+            }
+
         }
     }
 }
